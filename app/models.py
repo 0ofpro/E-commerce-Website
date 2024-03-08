ba2ae6bd357a73f05ca87596220393523a18df84
mondrita <144902427+mondrita@users.noreply.github.com>
2024-03-08 18:53:21 +0600

    implementing cart(add to cart, removie from cart, show total price)

--- app/models.py
+++ app/models.py
@@ -53,3 +53,13 @@ class Wishlist(db.Model):
 
     def __repr__(self):
         return f"<Wishlist(user_id='{self.user_id}', item_id='{self.item_id}')>"
+
+class Cart(db.Model):
+    __tablename__ = 'cart'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    item_id = db.Column(db.String(80), db.ForeignKey('item.Item_ID'), nullable=False)
+    item = db.relationship('Items', backref=db.backref('cart_items', lazy=True))
+
+    def __repr__(self):
+        return f"<Cart(user_id='{self.user_id}', item_id='{self.item_id}')>"
